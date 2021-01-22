f702c4c1e6430ce39abf5434aae5c674ba4b3a94
Marina Ercoli <68237165+Maercoli@users.noreply.github.com>
2021-01-21 22:54:33 -0500

    modifying code to be responsive

--- static/js/app.js
+++ static/js/app.js
@@ -4,12 +4,12 @@ function getDemoInfo(samples) {
     d3.json("./data/samples.json").then((data)=> {
     // get the metadata info for the demographic panel
         var metadata = data.metadata;
-        console.log(metadata)
+        //console.log(metadata)
         
         // filter meta data info by id
         var idArray = metadata.filter(sampleObject => sampleObject.id == samples);
         var id = idArray[0]
-        console.log(id)
+        //console.log(id)
         
         //select demographic panel to put data
         var demographicInfo = d3.select("#sample-metadata");      
@@ -17,33 +17,34 @@ function getDemoInfo(samples) {
         //clean demographic panel
         demographicInfo.html("")
 
-        //populate with result_inital
+        //populate with id
         Object.entries(id).forEach(([key, value])=>{
-            console.log(key, value)
+            //console.log(key, value)
             demographicInfo.append("h5").text(`${key}: ${value}`);
         });        
-
     });
 }
 
 
-function getPlots(samples) {
 
-    //Read samples.json
+// create function to get the necessary plots
+function getPlots(sample) {
+    //read samples.json
     d3.json("./data/samples.json").then (sampledata =>{
-        console.log(sampledata)
-            
-        //Grab values from response json object to build plots
-        var ids = sampledata[0].otu_ids;
-        console.log(ids)
-          
-        var sampleValues =  sampledata.samples[0].sample_values.slice(0,10).reverse();
+
+        console.log(sampledata) 
+        
+        //
+        var individualValues = sampledata.samples.filter(sampleObject => sampleObject.id == sample)[0];
+        //console.log(individualValues)
+
+        var sampleValues =  individualValues.sample_values.slice(0,10).reverse();
         console.log(sampleValues)
             
-        var labels =  sampledata.samples[0].otu_labels.slice(0,10);
+        var labels =  individualValues.otu_labels.slice(0,10);
         console.log (labels)
             
-        var OTU_top = (sampledata.samples[0].otu_ids.slice(0, 10)).reverse();
+        var OTU_top = (individualValues.otu_ids.slice(0, 10)).reverse();
             
         //get the otu id's to the desired form for the plot
         var OTU_id = OTU_top.map(d => "OTU " + d);
@@ -56,9 +57,9 @@ function getPlots(samples) {
             text: labels,
             type: "bar",
             orientation: "h"
-            }
+            };
             
-        var data = [trace]
+        var data = [trace];
 
         var layout = {
             title:"Top 10 OTUs",
@@ -69,60 +70,38 @@ function getPlots(samples) {
             b: 100
             }
         }
-            
+
         // Render the plot to the div tag with id "bar"
         Plotly.newPlot("bar", data, layout);
 
-        
         // Set trace for the bubble plot
         var trace1 = {
-            x: sampledata.samples[0].otu_ids,
-            y: sampledata.samples[0].sample_values,
+            x: individualValues.otu_ids,
+            y: individualValues.sample_values,
             mode: "markers",
             marker: {
-                size: sampledata.samples[0].sample_values,
-                color: sampledata.samples[0].otu_ids
+                size: individualValues.sample_values,
+                color: individualValues.otu_ids
             },
-            text:  sampledata.samples[0].otu_labels
-        };
+            text:  individualValues.otu_labels
+        }
         var layout_2 = {
             xaxis:{title: "OTU ID"},
             height: 600,
             width: 1000
-        };
+        }
         var data1 = [trace1];
         
         Plotly.newPlot("bubble", data1, layout_2);
         });
 
-        // Create trace for the gauge chart
-    //     var data = [
-    //         {
-    //           domain: { x: [0, 1], y: [0, 1] },
-    //           value: 450,
-    //           title: { text: "Belly Button Washing Frequency" },
-    //           type: "indicator",
-    //           mode: "gauge+number+delta",
-    //           delta: { reference: 380 },
-    //           gauge: {
-    //             axis: { range: [null, 500] },
-    //             steps: [
-    //               { range: [0, 250], color: "lightgray" },
-    //               { range: [250, 400], color: "gray" }
-    //             ],
-    //             threshold: {
-    //               line: { color: "red", width: 4 },
-    //               thickness: 0.75,
-    //               value: 490
-    //             }
-    //           }
-    //         }
-    //       ];
-          
-    //       var layout = { width: 600, height: 450, margin: { t: 0, b: 0 } };
-    //       Plotly.newPlot('gauge', data, layout);
+    }
 
-}
+
+//        // Create trace for the gauge chart
+       
+
+// }
 
 
 function init() {
